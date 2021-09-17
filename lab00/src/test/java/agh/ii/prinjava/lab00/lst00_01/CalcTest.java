ea4f9b0dda20293efdd5a562627d2e5ef3a138c2
qnoir <quentin.noiriel@efrei.net>
2021-09-17 13:52:16 +0200

    yo

--- lab00/src/test/java/agh/ii/prinjava/lab00/lst00_01/CalcTest.java
+++ lab00/src/test/java/agh/ii/prinjava/lab00/lst00_01/CalcTest.java
@@ -10,18 +10,21 @@ class CalcTest {
 
     @BeforeEach
     void setUp() {
+
         System.out.println("CalcTest.setUp...");
     }
 
     @AfterEach
     void tearDown() {
+
         System.out.println("CalcTest.tearDown...");
     }
 
     @Test
     void onePlusTwoIsThree() {
         // if
-        int a = 1, b = 2;
+        int a= 1, b = 2;
+
         // then
         assertEquals(3, Calc.add(a,b));
     }
